4087dd465c9abd8472d8268cfedce7f809f27200
Asaf Ohayon <asaf@sysbind.co.il>
2023-12-18 16:37:01 +0200

    making an http request

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,21 @@
-use std::env;
+use error_chain::error_chain;
+use std::io::Read;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    dbg!(args);
+error_chain! {
+    foreign_links {
+        Io(std::io::Error);
+        HttpRequest(reqwest::Error);
+    }
+}
+
+fn main() -> Result<()> {
+    let mut res = reqwest::blocking::get("http://localhost:15020/metrics")?;
+    let mut body = String::new();
+    res.read_to_string(&mut body)?;
+
+    println!("Status: {}", res.status());
+    println!("Headers:\n{:#?}", res.headers());
+    println!("Body:\n{}", body);
+
+    Ok(())
 }
